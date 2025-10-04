075d1322397c0a5e14916a22ba7199ddf9b1c649
AlaouyIsmail <ismail.alaouy@usmba.ac.ma>
2025-10-04 16:29:14 +0100

    Update Program.cs
    update

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,5 +1,19 @@
-﻿using System;namespace TestProject{	internal class Program	{		static void Main(string[] args){         for(int i = 0; i >= 10; i++)
+﻿using System;
+
+namespace TestProject
+{
+	internal class Program
+	{
+		static void Main(string[] args){
+         for(int i = 0; i >= 10; i++)
 			{
-			Console.Write("hello github");
+			Console.Write("hello github");
+			 Console.Write("hello github");
+			 Console.Write("hello github");
+			 Console.Write("hello github");
+
 			}
-            Console.ReadLine();					}	}}
+            Console.ReadLine();
+					}
+	}
+}
