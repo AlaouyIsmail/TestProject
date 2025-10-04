f47422e6fc4752860015b61fda44f1b5a66046a6
ismail <ismail@gmail.com>
2025-10-04 16:26:56 +0100

    add writing

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,2 +1,5 @@
-﻿using System;namespace TestProject{	internal class Program	{		static void Main(string[] args){			Console.Write("ismail alaouy");			Console.Write("ismail alaouy");			Console.Write("ismail alaouy");			Console.Write("ismail alaouy");			Console.Write("ismail alaouy");
+﻿using System;namespace TestProject{	internal class Program	{		static void Main(string[] args){         for(int i = 0; i >= 10; i++)
+			{
+			Console.Write("hello github");
+			}
             Console.ReadLine();					}	}}
